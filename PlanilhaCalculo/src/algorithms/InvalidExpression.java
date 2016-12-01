153ebc137be57eac55f50a6636163d41135e9e83
Luciano S. Junior <luciano.ssj@hotmail.com>
2016-12-01 15:33:17 -0200

    Delete InvalidExpression.java

--- PlanilhaCalculo/src/algorithms/InvalidExpression.java
+++
@@ -1,9 +0,0 @@
-package algorithms;
-
-public class InvalidExpression extends Exception {
-	private static final long serialVersionUID = -438560909159811265L;
-
-	public InvalidExpression(String msg) {
-		super("Invalid expression: " + msg);
-	}
-}
